aa9a3930a5aaba9cd577c6589fa3b3b5970d3a35
jim0watters <jim0watters@173e87ca-0341-0410-be3d-c70208e3ec33>
2008-01-31 19:04:23 +0000

    Fix issue with spaces in file names. Update VS 2003 project file

--- TWAIN-Samples/Twain_DS_sample01/visual_studio/stdafx.h
+++ TWAIN-Samples/Twain_DS_sample01/visual_studio/stdafx.h
@@ -78,7 +78,7 @@
 
 #include <afxdtctl.h>         // MFC support for Internet Explorer 4 Common Controls
 #ifndef _AFX_NO_AFXCMN_SUPPORT
-#include <afxcmn.h>           // MFC support for Windows Common Controls
+//#include <afxcmn.h>           // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 
